00debbf412b20b9e5d964d827f378b20ae12f134
crismolav <cmoraleso@utexas.edu>
2020-07-02 23:32:20 +0200

    sh cuda change

--- ggnn.sh
+++ ggnn.sh
@@ -6,17 +6,16 @@
 #SBATCH -e /homedtic/cmorales/log/%N.%J.task1.err # STDOUT
 # Number of GPUs per node
 #SBATCH --gres=gpu:1
-#SBATCH --mem=5G
 
 # export PATH="$HOME/project/anaconda3/bin:$PATH"
 # source activate goog3
 # cd /homedtic/cmorales/cmol/ggnn
 
-
-cd /homedtic/cmorales
-source tf1/bin/activate
+ml load Python/3.6.4-foss-2017a
 ml load TensorFlow/1.14.0-foss-2017a-Python-3.6.4
 ml load TensorFlow-gpu/1.14.0-foss-2017a-Python-3.6.4-CUDA-10.0.130
-module load CUDA/8.0.61
+module load CUDA/10.0.130
+cd /homedtic/cmorales
+source tf1/bin/activate
 cd cmol/ggnn
 python chem_tensorflow_dense.py --pr identity --restrict_data $1 --log_dir borrar $2
