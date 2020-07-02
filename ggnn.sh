86e8dcd274f45349bbdd6ba56079969f8cddcb3b
crismolav <cmoraleso@utexas.edu>
2020-07-02 19:08:03 +0200

    experiment bash

--- ggnn.sh
+++ ggnn.sh
@@ -8,7 +8,14 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem=5G
 
-export PATH="$HOME/project/anaconda3/bin:$PATH"
-source activate goog3
-cd /homedtic/cmorales/cmol/ggnn
+# export PATH="$HOME/project/anaconda3/bin:$PATH"
+# source activate goog3
+# cd /homedtic/cmorales/cmol/ggnn
+
+
+cd /homedtic/cmorales
+source tf1/bin/activate
+ml load TensorFlow/1.14.0-foss-2017a-Python-3.6.4
+ml load TensorFlow-gpu/1.14.0-foss-2017a-Python-3.6.4-CUDA-10.0.130
+cd /cmol/ggnn
 python chem_tensorflow_dense.py --pr identity --restrict_data $1 --log_dir borrar $2
