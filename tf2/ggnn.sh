89629b2065af9a4a65213ffbe7f99e46f642a9c9
crismolav <cmoraleso@utexas.edu>
2020-07-06 20:02:57 +0200

    sh to 2 gpus

--- tf2/ggnn.sh
+++ tf2/ggnn.sh
@@ -5,12 +5,9 @@
 #SBATCH -o /homedtic/cmorales/log/%N.%J.task1.out # STDOUT
 #SBATCH -e /homedtic/cmorales/log/%N.%J.task1.err # STDOUT
 # Number of GPUs per node
-#SBATCH --nodes=2
-#SBATCH --ntasks=2
-#SBATCH --gres=gpu:1
-#SBATCH --mem=10G
-# Select Intel nodes (with Infiniband)
-#SBATCH --constraint=intel
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:2
+#SBATCH --mem=20G
 
 export PATH="$HOME/project/anaconda3/bin:$PATH"
 source activate tfgpu
