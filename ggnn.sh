6d7dea2f9bddbd730ed968f48c83e6986aa5ce4b
crismolav <cmoraleso@utexas.edu>
2020-07-07 19:27:18 +0200

    updated tf1 bash script

--- ggnn.sh
+++ ggnn.sh
@@ -5,10 +5,11 @@
 #SBATCH -o /homedtic/cmorales/log/%N.%J.task1.out # STDOUT
 #SBATCH -e /homedtic/cmorales/log/%N.%J.task1.err # STDOUT
 # Number of GPUs per node
+#SBATCH --nodes=1
 #SBATCH --gres=gpu:1
+#SBATCH --mem=20G
 
-module load CUDA/10.0.130
-cd /homedtic/cmorales
-source tf1/bin/activate
-cd cmol/ggnn
-python chem_tensorflow_dense.py --pr identity --restrict_data $1 --log_dir borrar $2
+export PATH="$HOME/project/anaconda3/bin:$PATH"
+source activate tfgpu
+cd /homedtic/cmorales/cmol/ggnn
+python chem_tensorflow_dense.py --pr identity --restrict_data $1 --log_dir borrar $2 $3 $4
