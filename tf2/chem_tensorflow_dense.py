531256fc18c3c17378a8ec1512f575fff26cf81f
Cristian <cmoraleso@utexas.edu>
2020-08-16 23:22:27 +0200

    undo relu

--- tf2/chem_tensorflow_dense.py
+++ tf2/chem_tensorflow_dense.py
@@ -393,7 +393,7 @@ class DenseGGNNChemModel(ChemModel):
         # ID [e * b * v, 2h] else [b * v, 2h]
         last_h = tf.reshape(last_h, [-1, self.params["hidden_size"]])
         # ID [e * b * v, h] else [b * v, h]
-        gated_outputs = tf.nn.relu(regression_gate(gate_input)) * regression_transform(last_h)
+        gated_outputs = tf.nn.sigmoid(regression_gate(gate_input)) * regression_transform(last_h)
         # BTB [b * v, o] ID [e * b * v, o] else [b * v, 1]
 
         node_mask = self.placeholders['node_mask']
