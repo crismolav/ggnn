b6c2a6546ae9e17eb24018cc59729a92a7fca05c
Cristian <cmoraleso@utexas.edu>
2020-09-12 01:58:50 +0200

    first

--- tf2/chem_tensorflow.py
+++ tf2/chem_tensorflow.py
@@ -92,7 +92,7 @@ class ChemModel(object):
             'clamp_gradient_norm': 1.0,
             'out_layer_dropout_keep_prob': 0.95,
             'emb_dropout_keep_prob': 0.55,
-            'hidden_size': 350 if self.args['--pr'] not in ['identity'] else 350,
+            'hidden_size': 450 if self.args['--pr'] not in ['identity'] else 350,
             'num_timesteps': 4,
             'use_graph': True,
 
